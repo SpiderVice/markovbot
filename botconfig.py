becdff8f2132ab25887ea34ceead80932ea448a6
SpiderVice <spidervice@gmail.com>
2026-04-06 18:37:29 +0100

    feat(botconfig): add support for setting try count and state size as env variables

--- botconfig.py
+++ botconfig.py
@@ -1,7 +1,8 @@
-# Token for the bot, not to be confused with the account token
 import os
-
-TOKEN = os.environ.get("MARKOVBOT_TOKEN", "")
+# Token for the bot, not to be confused with the account token
+TOKEN = os.environ.get("MARKOVBOT_TOKEN", "") 
 BOT_CHANNEL = int(os.environ.get("MARKOVBOT_CHANNEL_ID", 0))
-TRY_COUNT = 50
-STATE_SIZE = 3
+
+TRY_COUNT = int(os.environ.get("TRY_COUNT", 50))
+STATE_SIZE = int(os.environ.get("STATE_SIZE", 3))
+
